a9993746161a02088a04cb756fb42a28e5441289
nxnaxx <noname1260@naver.com>
2021-11-08 23:59:28 +0900

    init git

---
+++ javascript_deep_dive/chapter06/dataType.js
@@ -0,0 +1,16 @@
+// Number Type
+var integer = 5;
+var double = 5.24;
+var negative = -20;
+
+// Number type은 모두 실수로 처리됨
+console.log(1 === 1.0);
+console.log(3 / 2);
+
+// Number type의 세 가지 특별한 값
+console.log(10 / 0);
+console.log(10 / -0);
+console.log(1 * 'String');
+
+// javascript는 대소문자를 구별함
+var x = nan;
