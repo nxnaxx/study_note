277754b69a2a2bc3f2d178a4de655cb645b8a7c9
nxnaxx <noname1260@naver.com>
2021-11-10 22:12:06 +0900

    Update 2 files in deep dive chapter07

---
+++ javascript_deep_dive/chapter07/logicalOperator.js
@@ -0,0 +1,20 @@
+// OR operator
+console.log(true || true);
+console.log(true || false);
+console.log(false || false);
+
+// AND operator
+console.log(true && true);
+console.log(true && false);
+console.log(false && false);
+
+// NOT operator
+console.log(!true);
+console.log(!false);
+
+// implicit coercion
+console.log(!0);
+console.log(!'carrot');
+
+// short-circuit evaluation
+console.log('Dog' && 'Cat');
---
+++ javascript_deep_dive/chapter07/otherOperator.js
@@ -0,0 +1,53 @@
+// ternary operator
+var x = 5;
+var result = x % 2 ? '홀수' : '짝수';
+
+console.log(result);
+
+// comma operator
+var x, y, z;
+
+x = 1, y = 2, z = 3;
+
+// group operator
+console.log(10 * 3 + 5);
+console.log(10 * (3 + 5));
+
+// typeof operator
+typeof ''
+typeof 1
+typeof NaN
+typeof true
+typeof undefined
+typeof Symbol()
+typeof null
+typeof []
+typeof {}
+typeof new Date()
+typeof /test/gi
+typeof function () {}
+
+// typeof null
+var sample = null;
+
+typeof sample === null;
+sample === null;
+
+// exponent operator
+console.log(2 ** 2);
+console.log(2 ** 2.5);
+console.log(2 ** 0);
+console.log(2 ** -4);
+console.log(2 ** 2 ** 2);
+
+var num = 7;
+num **= 2;
+console.log(num);
+
+console.log(2 * 5 ** 3);
+
+// Math.pow method
+Math.pow(2, 2);
+Math.pow(2, 2.5);
+Math.pow(Math.pow(2, 2), 2);
+
