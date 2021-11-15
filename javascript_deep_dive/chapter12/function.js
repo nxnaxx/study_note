897b4cd40a9fa000518440793f456bc9f0699bc3
nxnaxx <noname1260@naver.com>
2021-11-15 23:59:34 +0900

    Update deep dive chapter12

--- javascript_deep_dive/chapter12/function.js
+++ javascript_deep_dive/chapter12/function.js
@@ -4,3 +4,31 @@ function mul(a, b) {
 }
 
 console.log(mul(3, 5));
+
+// function declaration
+function add(a, b) {
+  return a + b;
+}
+
+console.dir(add);
+console.log(add(4, 6));
+
+// function expression
+var sub = function (x, y) {
+  return x - y; 
+};
+
+console.log(sub(10, 5));
+
+// function declaration VS function expression
+// function call
+console.log(declAdd(4, 6));
+// console.log(expAdd(4, 6)); // TypeError
+
+function declAdd(x, y) {
+  return x + y;
+}
+
+var expAdd = function (x, y) {
+  return x + y;
+};
