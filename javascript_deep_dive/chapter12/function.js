0a0dbbe6b0dd33e73966e15777688601bdde1071
nxnaxx <noname1260@naver.com>
2021-11-16 17:46:39 +0900

    Update deep dive chapter12

--- javascript_deep_dive/chapter12/function.js
+++ javascript_deep_dive/chapter12/function.js
@@ -32,3 +32,75 @@ function declAdd(x, y) {
 var expAdd = function (x, y) {
   return x + y;
 };
+
+// arrow function
+const div = (x, y) => x / y;
+console.log(div(10, 8));
+
+// IIFE
+(function () {
+  var x = 8;
+  var y = 5;
+  return x * y;
+}());
+
+var immed = (function () {
+  var x = 8;
+  var y = 5;
+  return x * y;
+}());
+
+console.log(immed);
+
+immed = (function (a, b) {
+  return a * b;
+}(8, 5));
+
+console.log(immed);
+
+// recursive function
+function countDown(num) {
+  if (num === 0) return ;
+  console.log(num);
+  countDown(num - 1);
+}
+
+countDown(5);
+
+function fact(n) {
+  if (n === 1) return 1;
+  return n * fact(n - 1);
+}
+
+console.log(`1! = ${fact(1)}`);
+console.log(`2! = ${fact(2)}`);
+console.log(`3! = ${fact(3)}`);
+console.log(`4! = ${fact(4)}`);
+console.log(`5! = ${fact(5)}`);
+
+// nested function
+function outer() {
+  var a = 5;
+
+  function inner() {
+    var b = 3;
+    console.log(a + b);
+  }
+
+  inner();
+}
+
+outer();
+
+// callback function
+function odd(num, cb) {
+  for (i = 1; i < num; i++) {
+    cb(i);
+  }
+}
+
+var high = function (i) {
+  if (i % 2 !== 0) console.log(i);
+};
+
+odd(10, high);
