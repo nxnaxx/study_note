c027ccded1cd57759ab5af8ec6995898f29c79f6
nxnaxx <noname1260@naver.com>
2021-11-19 00:53:04 +0900

    Update deep dive chapter19

---
+++ javascript_deep_dive/chapter19/prototype_chain.js
@@ -0,0 +1,20 @@
+// prototype chain
+function Hobby(sport) {
+  this.sport = sport;
+}
+
+Hobby.prototype.intro = function() {
+  console.log(`My hobby is ${this.sport}`);
+};
+
+const me = new Hobby('futsal');
+
+console.log(me.hasOwnProperty('sport'));
+
+// instanceof operator
+const parent = {};
+
+Object.setPrototypeOf(me, parent);
+
+console.log(me instanceof Hobby);
+console.log(me instanceof Object);
